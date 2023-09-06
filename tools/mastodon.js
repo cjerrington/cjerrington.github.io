4267001263fc5d28835e0865a66929b8c6cdf46a
Clayton Errington <me@claytonerrington.com>
2023-09-06 11:30:31 -0500

    add vercel.json and webfinger

---
+++ tools/mastodon.js
@@ -0,0 +1,10 @@
+const pullMyWebFinger = async ( domain, user ) => {
+    const url = `https://${ domain }/.well-known/webfinger?resource=acct:${ user }@${ domain }`;
+    const res = await fetch( url );
+    const body = await res.text();
+    const profile = JSON.parse( body );
+    console.log( profile );
+  };
+  
+  pullMyWebFinger( "mstdn.social", "cjerrington" );
+  
