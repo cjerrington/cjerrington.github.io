e10ca47c1b42e0d0efaaac93896112297cab7812
Clayton Errington <me@claytonerrington.com>
2023-09-25 12:14:48 -0500

    add last update time for blogroll

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -2,5 +2,9 @@ module.exports = {
   currentYear() {
     const today = new Date();
     return today.getFullYear();
+  },
+  buildTime() { 
+    const today = new Date().toLocaleString();
+    return today;
   }
 };
