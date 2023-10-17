728520af14b874a424250234da0b895c64709203
Clayton Errington <me@claytonerrington.com>
2023-10-17 16:27:28 -0500

    update tina config

--- tina/config.ts
+++ tina/config.ts
@@ -160,6 +160,144 @@ export default defineConfig({
           
         },
       },
+      {
+        name: "drafts",
+        label: "Drafts",
+        path: "src/drafts",
+        format: 'md',
+        defaultItem: () => {
+          return {
+            // When a new post is created the title field will be set to "New post"
+            date: dateTime,
+            draft: true,
+            tags: "100DaysToOffload"
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'string',
+            label: 'Excerpt',
+            name: 'excerpt',
+          },
+          {
+            name: 'draft',
+            label: 'Draft',
+            type: 'boolean',
+            required: true,
+            description: 'If this is checked the post will not be published',
+          },
+          {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+            ui: {
+              dateFormat: 'YYYY-MM-DD',
+              
+            },
+          },
+          {
+            type: 'string',
+            label: 'Tags',
+            name: 'tags',
+            list: true,
+            options: [
+              {
+                value: "100DaysToOffload",
+                label: "100DaysToOffload"
+              },
+              {
+                value: "11ty",
+                label: "11ty"
+              }, 
+              {
+                value: "automation",
+                label: "Automation"
+              }, 
+              {
+                value: "aws",
+                label: "AWS"
+              },
+              {
+                value: "cloud",
+                label: "Cloud"
+              },
+              {
+                value: "docker",
+                label: "Docker"
+              },
+              {
+                value: "git",
+                label: "Git"
+              },
+              {
+                value: "linux",
+                label: "Linux"
+              },
+              {
+                value: "powershell",
+                label: "PowerShell"
+              },
+              {
+                value: "python",
+                label: "Python"
+              },
+              {
+                value: "selfhost",
+                label: "Selfhost"
+              },
+              {
+                value: "shell",
+                label: "Shell"
+              },
+              {
+                value: "snippets",
+                label: "Snippets"
+              },
+              {
+                value: "web design",
+                label: "Web Design"
+              },
+              {
+                value: "windows",
+                label: "Windows"
+              },
+              {
+                value: "wordle",
+                label: "Wordle"
+              },
+              
+            ]
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          
+          filename: {
+            // if disabled, the editor can not edit the filename
+            readonly: true,
+            // Example of using a custom slugify function
+            slugify: (values) => {
+              // Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
+              return `${dateTime}-${values.title
+                ?.toLowerCase()
+                .replace(/ /g, '-')}`
+            },
+          },
+          
+        },
+      },
     ],
   },
 });
